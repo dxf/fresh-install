66bafe037bc3b5b4dd3fcdd0cb1463bed68a6c53
Dafydd <me@daf.lol>
2020-02-25 11:58:12 +0000

    i shouldnt be up that late

--- choco-daily-update.ps1
+++ choco-daily-update.ps1
@@ -7,7 +7,7 @@ cd C:\tempdl
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/nadia-moon/chocolatey/master/program_list.config" -OutFile "C:\tempdl\program_list.config"
 choco install program_list.config -y
-$Trigger= New-ScheduledTaskTrigger -At 10:00pm -Daily
+$Trigger= New-ScheduledTaskTrigger -At 09:00pm -Daily
 $User= "NT AUTHORITY\SYSTEM"
 $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "choco upgrade all -y"
 Register-ScheduledTask -TaskName "daily_choco_update" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
