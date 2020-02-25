4be9eae85c505cf815caba27f643f1d70103dce5
Dafydd <me@daf.lol>
2020-02-25 12:03:00 +0000

    Update choco-daily-update.ps1

--- choco-daily-update.ps1
+++ choco-daily-update.ps1
@@ -1,11 +1,11 @@
 #Requires -RunAsAdministrator
 <#
-script to install packages from https://github.com/nadia-moon/chocolatey, and creates a scheduled daily task to update software
+script to install packages from https://github.com/dxf/chocolatey, and creates a scheduled daily task to update software
 #>
 mkdir c:\tempdl
 cd C:\tempdl
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/nadia-moon/chocolatey/master/program_list.config" -OutFile "C:\tempdl\program_list.config"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dxf/chocolatey/master/program_list.config" -OutFile "C:\tempdl\program_list.config"
 choco install program_list.config -y
 $Trigger= New-ScheduledTaskTrigger -At 09:00pm -Daily
 $User= "NT AUTHORITY\SYSTEM"
