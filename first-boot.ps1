ff2ccebc1869b8ab302976db6b28f00f3929abb2
Dafydd <me@daf.lol>
2020-02-25 11:46:08 +0000

    Create first-boot.ps1

---
+++ first-boot.ps1
@@ -0,0 +1,4 @@
+#Requires -RunAsAdministrator
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$script = $PSScriptRoot+"\package_install.ps1"
+& $script
