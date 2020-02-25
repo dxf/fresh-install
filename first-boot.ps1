1fa016ff079144a9f208167d51f956f61f8f77c8
Dafydd <me@daf.lol>
2020-02-25 11:59:02 +0000

    Update first-boot.ps1

--- first-boot.ps1
+++ first-boot.ps1
@@ -1,4 +1,8 @@
 #Requires -RunAsAdministrator
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-$script = $PSScriptRoot+"\package_install.ps1"
-& $script
+$installscript = $PSScriptRoot+"\package_install.ps1"
+& $installscript
+$telescript = $PSScriptRoot+"\block-telemetry.ps1"
+& $telescript
+$dailyupdate = $PSScriptRoot+"\choco-daily-update.ps1"
+& $dailyupdate
