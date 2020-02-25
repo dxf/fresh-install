25a2e058a1a46a6eefd9090a15856fbfc4ce8096
Dafydd <me@daf.lol>
2020-02-25 12:06:04 +0000

    Add TLS 1.2 Support

--- first-boot.ps1
+++ first-boot.ps1
@@ -1,4 +1,7 @@
 #Requires -RunAsAdministrator
+if (-not ([Net.ServicePointManager]::SecurityProtocol).ToString().Contains([Net.SecurityProtocolType]::Tls12)) {
+  [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol.toString() + ', ' + [Net.SecurityProtocolType]::Tls12
+}
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 $installscript = $PSScriptRoot+"\package_install.ps1"
 & $installscript
